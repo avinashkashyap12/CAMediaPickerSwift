c61a943d6fb8e82512d3eeb0b3b6d6ec7416f822
Avinash Kashyap <iavinashkashyap@gmail.com>
2018-04-29 18:20:13 +0530

    Initial Commit

---
+++ CAMediaPickerExample/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CAMediaPickerExample
+//
+//  Created by Avinash Kashyap on 29/04/18.
+//  Copyright © 2018 Avinash Kashyap. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
